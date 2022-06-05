b19655802565cfa55c913dd457717e63712afb0d
Arteks718 <artexbro.official@gmail.com>
2022-06-05 14:33:57 +0300

    05

--- js/main.js
+++ js/main.js
@@ -26,54 +26,54 @@ var app = new Vue({
         addItem:function(id){
             window.localStorage.setItem('prod',id)
         },
-        // getProducts:function(){
-        //     if(window.location.hash) {
-        //         var id = window.location.hash.replace('#','');
-        //         if(this.products && this.products) {
-        //            for(i in this.products) {
-        //                if(this.products[i] && this.products[i].id && id==this.products[i].id) this.product=this.products[i];
-        //            } 
-        //         }
-        //     }
-        // },
-        // addToCart:function(id) {
-        //     var cart = [];
-        //     if(window.localStorage.getItem('cart')) {
-        //         cart = window.localStorage.getItem('cart').split(',');
-        //     }
-        //     if(cart.indexOf(String(id))==-1) {
-        //         cart.push(id);
-        //         window.localStorage.setItem('cart', cart.join());
-        //         this.btnVisible=1;
-        //     }
-        // },
-        // checkInCart:function() {
-        //     if(this.product && this.product.id && window.localStorage.getItem('cart').split(',').indexOf(String(this.product.id)) !=-1 ) this.btnVisible=1;
-        // },
-        // getCart:function() {
-        //     for(i in localStorage.getItem('cart')) {
-        //         for(p in this.products) {
-        //             if(this.products[p].id == localStorage.getItem('cart').split(',')[i]) {
-        //                this.cart.push(this.products[p]);
-        //                this.cartIds.push(this.products[p].id);
-        //             }
-        //         }
-        //     }
-        // },
-        // removeFromCart:function(id) {
-        //     for(i in this.cart) {
-        //         if(this.cart[i].id == id) {
-        //             this.cart.splice(i, 1);
-        //             this.cartIds.splice(i, 1);
-        //             window.localStorage.setItem('cart', this.cartIds.join());
-        //         }
-        //     }
-        // },
-        // makeOrder:function() {
-        //     this.orderVisible = 1;
-        //     this.cart = [];
-        //     this.cartIds = [];
-        //     window.localStorage.removeItem('cart');
-        // }
+        getProducts:function(){
+            if(window.location.hash) {
+                var id = window.location.hash.replace('#','');
+                if(this.products && this.products) {
+                   for(i in this.products) {
+                       if(this.products[i] && this.products[i].id && id==this.products[i].id) this.product=this.products[i];
+                   } 
+                }
+            }
+        },
+        addToCart:function(id) {
+            var cart = [];
+            if(window.localStorage.getItem('cart')) {
+                cart = window.localStorage.getItem('cart').split(',');
+            }
+            if(cart.indexOf(String(id))==-1) {
+                cart.push(id);
+                window.localStorage.setItem('cart', cart.join());
+                this.btnVisible=1;
+            }
+        },
+        checkInCart:function() {
+            if(this.product && this.product.id && window.localStorage.getItem('cart').split(',').indexOf(String(this.product.id)) !=-1 ) this.btnVisible=1;
+        },
+        getCart:function() {
+            for(i in localStorage.getItem('cart')) {
+                for(p in this.products) {
+                    if(this.products[p].id == localStorage.getItem('cart').split(',')[i]) {
+                       this.cart.push(this.products[p]);
+                       this.cartIds.push(this.products[p].id);
+                    }
+                }
+            }
+        },
+        removeFromCart:function(id) {
+            for(i in this.cart) {
+                if(this.cart[i].id == id) {
+                    this.cart.splice(i, 1);
+                    this.cartIds.splice(i, 1);
+                    window.localStorage.setItem('cart', this.cartIds.join());
+                }
+            }
+        },
+        makeOrder:function() {
+            this.orderVisible = 1;
+            this.cart = [];
+            this.cartIds = [];
+            window.localStorage.removeItem('cart');
+        }
     }
 });
